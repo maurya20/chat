c0d982c6d334014a1b87792950cffb347f3cc4f7
maurya20 <mukesh.ice17@gmail.com>
2021-09-29 20:09:26 +0530

    update in ui

--- client/src/App.js
+++ client/src/App.js
@@ -7,35 +7,38 @@ const socket = io.connect("http://localhost:4000", {
   transports: ["websocket"],
 });
 function App() {
-  const [state, setState] = useState({ message: "", name: "" });
+  const [state, setState] = useState("");
   const [chat, setChat] = useState([]);
   const [user, setUser] = useState("");
   console.log("/////", user);
   useEffect(() => {
     socket.on("message", ({ name, message }) => {
-      setChat([...chat, { name: user, message }]);
+      setChat([...chat, { name, message }]);
     });
   });
 
   const onTextChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value });
+    setState(e.target.value);
   };
 
   const onMessageSubmit = (e) => {
     e.preventDefault();
-    const { name, message } = state;
-    socket.emit("message", { name, message });
-    setState({ message, name });
+    socket.emit("message", { name: user, message: state });
+    setState("");
   };
 
   const renderChat = () => {
-    return chat.map(({ name, message }, index) => (
-      <div key={index}>
-        <h3>
-          {name}: <span>{message}</span>
-        </h3>
+    return (
+      <div style={{ overflowY: "auto", minHeight: "500px" }}>
+        {chat.map(({ name, message }, index) => (
+          <div key={index} className="mb-3">
+            <h6>
+              {name}: <span>{message}</span>
+            </h6>
+          </div>
+        ))}
       </div>
-    ));
+    );
   };
 
   return (
@@ -43,23 +46,37 @@ function App() {
       <Appbar user={user} />
       <div className="container mt-5">
         {user === "" ? <UserName setUser={setUser} /> : renderChat()}
-        <form onSubmit={onMessageSubmit}>
-          <div className="input-group mb-3">
-            <span className="input-group-text">Message:</span>
-            <input
-              type="text"
-              className="form-control"
-              aria-label="Amount (to the nearest dollar)"
-              name="message"
-              onChange={(e) => onTextChange(e)}
-              value={state.message}
-              id="outlined-multiline-static"
-            />
-            <button className="btn btn-success" id="basic-addon2" type="submit">
-              Send
-            </button>
-          </div>
-        </form>
+        {user && (
+          <form onSubmit={onMessageSubmit}>
+            <div
+              className="input-group mb-3"
+              style={{
+                position: "fixed",
+                width: "80%",
+                bottom: 0,
+                height: "2.5rem",
+              }}
+            >
+              <span className="input-group-text">Message:</span>
+              <input
+                type="text"
+                className="form-control"
+                aria-label="Amount (to the nearest dollar)"
+                name="message"
+                value={state}
+                onChange={onTextChange}
+                id="outlined-multiline-static"
+              />
+              <button
+                className="btn btn-success"
+                id="basic-addon2"
+                type="submit"
+              >
+                Send
+              </button>
+            </div>
+          </form>
+        )}
         <div className="render-chat"></div>
       </div>
     </>
@@ -67,3 +84,77 @@ function App() {
 }
 
 export default App;
+
+// import React, { useEffect, useState } from "react";
+// import io from "socket.io-client";
+// import "./App.css";
+// const socket = io.connect("http://localhost:4000", {
+//   transports: ["websocket"],
+// });
+// function App() {
+//   const [state, setState] = useState({ message: "", name: "" });
+//   const [chat, setChat] = useState([]);
+//   console.log("/////", chat);
+//   useEffect(() => {
+//     socket.on("message", ({ name, message }) => {
+//       setChat([...chat, { name, message }]);
+//     });
+//   });
+
+//   const onTextChange = (e) => {
+//     setState({ ...state, [e.target.name]: e.target.value });
+//   };
+
+//   const onMessageSubmit = (e) => {
+//     e.preventDefault();
+//     const { name, message } = state;
+//     socket.emit("message", { name, message });
+//     setState({ message, name });
+//   };
+
+//   const renderChat = () => {
+//     return chat.map(({ name, message }, index) => (
+//       <div key={index}>
+//         <h3>
+//           {name}: <span>{message}</span>
+//         </h3>
+//       </div>
+//     ));
+//   };
+
+//   return (
+//     <div className="card">
+//       <form onSubmit={onMessageSubmit}>
+//         <h1>Messenger</h1>
+//         <div className="name-field">
+//           <span>Name:</span>{" "}
+//           <input
+//             name="name"
+//             onChange={(e) => onTextChange(e)}
+//             value={state.name}
+//             label="Name"
+//           />
+//         </div>
+//         <div>
+//           {" "}
+//           <span>Message:</span>{" "}
+//           <input
+//             name="message"
+//             onChange={(e) => onTextChange(e)}
+//             value={state.message}
+//             id="outlined-multiline-static"
+//             variant="outlined"
+//             label="Message"
+//           />
+//         </div>
+//         <button type="submit">Send Message</button>
+//       </form>
+//       <div className="render-chat">
+//         <h1>Chat Log</h1>
+//         {renderChat()}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-// app.use(cors());
-// app.options("*", cors()); // enable pre-flight
+const path = require("path");
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 io.on("connection", (socket) => {
   socket.on("message", ({ name, message }) => {
     io.emit("message", { name, message });
